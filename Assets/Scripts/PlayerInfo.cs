6d6603f75cecb3983d7c809e7a6213dcfe3f6e7a
Dylan Domen <dylan.domen@gmail.com>
2022-05-05 16:55:59 +0100

    player name remove useless code

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -11,20 +11,18 @@ public class PlayerInfo : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(this.GetComponent<PhotonView>().ViewID);
-        PhotonNetwork.NickName = "Player " + this.GetComponent<PhotonView>().ViewID;
-        Debug.Log(PhotonNetwork.NickName);
-        //this.GetComponentInChildren<Canvas>().GetComponentInChildren<TextMesh>().text = PhotonNetwork.NickName;
-        //this.transform.Find("Canvas").transform.Find("TextUsername").GetComponent<TextMesh>().text = PhotonNetwork.NickName;
-        //this.GetComponentInChildren<TextMesh>().text = PhotonNetwork.NickName;
-        //this.GetComponentInChildren<this>.GetComponentInChildren<TextMeshPro>().text = PhotonNetwork.NickName;
-        //this.transform.GetChild(0).Find("TextUsername").gameObject.GetComponent<TextMeshPro>().text = PhotonNetwork.NickName;
-        textUsername.text = PhotonNetwork.NickName;
+        showPlayerName();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void showPlayerName()
+    {
+        PhotonNetwork.NickName = "Player " + this.GetComponent<PhotonView>().ViewID;
+        textUsername.text = PhotonNetwork.NickName;
     }
 }
